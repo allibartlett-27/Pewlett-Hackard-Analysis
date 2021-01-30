a666b60ba88ddfd95ad5365d2858d09fc7047d92
Allison Bartlett <allibartlett.27@gmail.com>
2021-01-30 17:30:32 -0600

    Queries folder

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,13 @@
+-- Delieverable 1 
+SELECT e.emp_no, 
+e.first_name,
+e.last_name,
+ti.title, 
+ti.from_date,
+ti.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
