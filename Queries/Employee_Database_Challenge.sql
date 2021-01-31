7dba9fe5e5dd9c4780893941df1c7abd70f79923
Allison Bartlett <allibartlett.27@gmail.com>
2021-01-30 18:41:22 -0600

    SQL code for D1

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -11,3 +11,17 @@ LEFT JOIN titles as ti
 ON e.emp_no = ti.emp_no
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no;
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY emp_no, to_date DESC;
+-- Number About to Retire
+SELECT COUNT (title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT DESC;
